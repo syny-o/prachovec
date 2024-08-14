28ccaa9100ee8d57e423fa3a252a761988a2b5ac
ondrejsynek <ondrej.synek@virtuell.cz>
2024-08-14 09:54:02 +0200

    first part

---
+++ prachovec/prachovec/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for prachovec project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prachovec.settings')
+
+application = get_wsgi_application()
