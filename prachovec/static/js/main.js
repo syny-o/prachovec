dfea97184d384caf25a9970af6c45e55936af942
syny-o <synek.o@seznam.cz>
2024-08-24 16:16:23 +0200

    adjustment for mobile devices

--- prachovec/static/js/main.js
+++ prachovec/static/js/main.js
@@ -2,16 +2,18 @@
 
 function scrollEffect() {
   const navbar = document.querySelector(".navbar");
+  const navbarImg = document.querySelector(".navbar-brand");
 
   window.addEventListener("scroll", () => {
     if (window.scrollY > 50) {
       navbar.classList.add("navbar-scroll");
+      navbarImg.classList.add("logo-small");
     } else {
       navbar.classList.remove("navbar-scroll");
+      navbarImg.classList.remove("logo-small");
     }
   });
 }
-
 
 // Go to Top Button
 function goToTop() {
@@ -35,8 +37,6 @@ function hideSpinner() {
     spinnerWrapper.remove();
   }, 500);
 }
-
-  
 
 document.addEventListener("DOMContentLoaded", () => {
   scrollEffect();
