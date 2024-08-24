69958e5d16c84916d7ad46e2d5e5d6b62d56fba3
syny-o <synek.o@seznam.cz>
2024-08-24 16:15:56 +0200

    news server rendering

--- prachovec/mysite/models.py
+++ prachovec/mysite/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 # Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=200)
+    text = RichTextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-date']
+        verbose_name = 'Aktualita'
+        verbose_name_plural = 'Aktuality'
+        
+
+    def __str__(self):
+        return self.title
+
+    
+
--- prachovec/mysite/views.py
+++ prachovec/mysite/views.py
@@ -2,9 +2,20 @@ from django.shortcuts import render, redirect
 from django.core.mail import send_mail
 from django.conf import settings
 
+from .models import News
+
 
 def home(request):
-    return render(request, 'mysite/home.html')
+    news = News.objects.all()
+    if len(news) > 2:
+        news = news[:2]
+        
+    context = {
+        'news': news
+    }
+
+
+    return render(request, 'mysite/home.html', context)
 
 
 def ubytovani(request):
