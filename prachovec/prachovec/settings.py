9414a47b4bbf46db0c6bb4b81d39c161e9c60925
syny-o <synek.o@seznam.cz>
2024-12-19 17:16:55 +0100

    changed dir for templates

--- prachovec/prachovec/settings.py
+++ prachovec/prachovec/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-82t2*auan4w#em&%5487dw5=p^f(tejm9c!z%uefmz-t1y)jr&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost',]
 
 
 # Application definition
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'prachovec.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
