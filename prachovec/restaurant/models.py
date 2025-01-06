766cfa9994f7d40ed572de96cfb1f183c338ac5d
syny-o <synek.o@seznam.cz>
2025-01-06 10:46:53 +0100

    added possibility to set position of items in model/template

--- prachovec/restaurant/models.py
+++ prachovec/restaurant/models.py
@@ -2,46 +2,61 @@ from django.db import models
 
 # Food Models
 class FoodCategory(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255, unique=True, verbose_name = "Název")
+    position = models.PositiveIntegerField(default=10, verbose_name = "Pozice v tabulce", help_text="Position for ordering categories in the final template. Lower values appear first.")
 
     def __str__(self):
         return self.name
     
     class Meta:
         verbose_name = 'Jídelní lístek - kategorie'      
+        verbose_name_plural = 'Jídelní lístek - kategorie'
+        ordering = ['position']  # Categories will be ordered by the 'position' field
+ 
 
 
 class FoodItem(models.Model):
-    category = models.ForeignKey(FoodCategory, related_name='items', on_delete=models.CASCADE)
+    category = models.ForeignKey(FoodCategory, related_name='items', on_delete=models.CASCADE, verbose_name = "Kategorie")
     name = models.CharField(max_length=255)
-    price = models.CharField(max_length=50, default='---')  # Price as string if not numeric
+    price = models.CharField(max_length=50, default='-')
+    position = models.PositiveIntegerField(default=10, verbose_name = "Pozice v tabulce")
 
     def __str__(self):
         return f"{self.name} - {self.price}"
     
     class Meta:
         verbose_name = 'Jídelní lístek - produkty'      
+        verbose_name_plural = 'Jídelní lístek - produkty'    
+        ordering = ['position']    
 
 
 # Drink Models
 class DrinkCategory(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255, unique=True, verbose_name = "Název")
+    position = models.PositiveIntegerField(default=10, verbose_name = "Pozice v tabulce", help_text="Position for ordering categories in the final template. Lower values appear first.")
 
     def __str__(self):
         return self.name
     
     class Meta:
         verbose_name = 'Nápojový lístek - kategorie'    
+        verbose_name_plural = 'Nápojový lístek - kategorie'  
+        ordering = ['position']  
 
 
 class DrinkItem(models.Model):
-    category = models.ForeignKey(DrinkCategory, related_name='items', on_delete=models.CASCADE)
+    category = models.ForeignKey(DrinkCategory, related_name='items', on_delete=models.CASCADE, verbose_name = "Kategorie")
     name = models.CharField(max_length=255)
-    price = models.CharField(max_length=50, default='---')  # Price as string if not numeric
+    price = models.CharField(max_length=50, default='-')
+    position = models.PositiveIntegerField(default=10, verbose_name = "Pozice v tabulce")
 
     def __str__(self):
         return f"{self.name} - {self.price}"
     
 
     class Meta:
-        verbose_name = 'Nápojový lístek - produkty'   
+        verbose_name = 'Nápojový lístek - produkty'
+        verbose_name_plural = 'Nápojový lístek - produkty'
+        ordering = ['position']  
+
+         
