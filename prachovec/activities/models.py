0b2d3c5249b06b4a132ef526bf97f0340632d146
syny-o <synek.o@seznam.cz>
2025-01-07 18:51:06 +0100

    added photo gallery to admin side

--- prachovec/activities/models.py
+++ prachovec/activities/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+
+class Photo(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Název')
+    image = models.ImageField(upload_to='activities_photos/', verbose_name='Obrázek')
+    description = RichTextField(verbose_name='Popisek')
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = 'Fotka'
+        verbose_name_plural = 'Galerie'  
+    
+
+    def delete(self, *args, **kwargs):
+        self.image.delete()
+        super(Photo, self).delete(*args, **kwargs)
--- prachovec/activities/views.py
+++ prachovec/activities/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from .models import Photo
 from .data import places
 
 
 def activities(request):
+    # photo Gallery
+    photos = Photo.objects.all()
+
     context = {
         'places': places,
+        'photos': photos,
     }
     return render(request, 'activities/activities.html', context)
